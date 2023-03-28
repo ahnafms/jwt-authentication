37742e4bf1bbc8cd6f6f6edec524e58de7ccf7da
ahnafms <ahnafmusyaffa47@gmail.com>
2023-03-28 20:41:44 +0700

    fix: uppercase components register and login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,22 +1,36 @@
 import React from "react";
-import { useForm } from "react-hook-form"; 
+import { useForm } from "react-hook-form";
 
-
-export default function login() {
-  const {register, handleSubmit} = useForm()
+export default function Login() {
+  const { register, handleSubmit } = useForm();
   const onSubmit = (data) => {
-  console.log(data)
+    console.log(data);
   };
   return (
     <main className="h-screen w-full flex-col flex justify-center items-center">
       <div className="flex flex-col justify-center items-start gap-y-5 p-9 rounded-xl border-slate-800 border-2">
         <p className="text-center w-full">Login</p>
-        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-y-5">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col gap-y-5"
+        >
           <label htmlFor="nama">Nama</label>
-          <input {...register("nama")} type="text" id="nama" className="px-4 py-2 rounded-sm"/>
+          <input
+            {...register("nama")}
+            type="text"
+            id="nama"
+            className="px-4 py-2 rounded-sm"
+          />
           <label htmlFor="password">Password</label>
-          <input {...register("password")} type="text" id="password" className="px-4 py-2 rounded-sm"/>
-          <button className="p-3" type="submit">Submit</button>
+          <input
+            {...register("password")}
+            type="text"
+            id="password"
+            className="px-4 py-2 rounded-sm"
+          />
+          <button className="p-3" type="submit">
+            Submit
+          </button>
         </form>
       </div>
     </main>
